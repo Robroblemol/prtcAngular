181d5b0b0b187c5c97046e4d367861a32719537c
Raziel <robroblemol>
2018-09-28 00:26:36 -0500

    working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-
+  imp: number;
+  por: number = 10;
+  res: number = 0;
+  flag_p: boolean;
+  calculated() {
+    if (this.flag_p == true) {
+      console.log(this.imp + (this.imp*this.por)/100)
+      this.res = this.imp + (this.imp*this.por)/100;
+    }else{
+      console.log(this.imp)
+      this.res = this.imp;
+    }
+  }
+  checkPro(){
+    console.log(this.flag_p)
+    if(this.flag_p)this.flag_p =false;
+    else this.flag_p = true;
+    console.log(this.flag_p)
+  }
   constructor() { }
 
   ngOnInit() {
